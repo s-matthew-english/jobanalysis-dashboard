1c2993e057a3c3de9fa2c6b7c64f32fe60be4696
ErikNovak <novak.erik@ijs.si>
2016-07-06 14:31:11 +0200

    Changed left margin of histograms, so that the whole text is inside the window

--- public/js/graphCreation.js
+++ public/js/graphCreation.js
@@ -15,26 +15,26 @@ var dateLineplot = new Lineplot({
     container:  "#analytics-chart-jobs-by-dates",
     identifier: "date",
     chartTitle: "Job Posts by Dates",
-    margin:     { top: 50, left: 30, bottom: 100, right: 20 }
+    margin:     { top: 50, left: 40, bottom: 100, right: 20 }
 });
 
 // Creates the skill Histogram
 var skillHistogram = new Histogram({
     container: "#analytics-chart-skills-by-frequency",
     name:      "skill",
-    margin:    { top: 80, left: 30, bottom: 100, right: 20 }
+    margin:    { top: 80, left: 40, bottom: 100, right: 20 }
 });
 
 // Creates the location Histogram
 var locationHistogram = new Histogram({
     container: "#analytics-chart-location-by-frequency",
     name:      "location",
-    margin:    { top: 80, left: 30, bottom: 100, right: 20 }
+    margin:    { top: 80, left: 40, bottom: 100, right: 20 }
 });
 
 // Creates the countries Histogram
 var countriesHistogram = new Histogram({
     container: "#analytics-chart-countries-by-frequency",
     name:      "countries",
-    margin:    { top: 80, left: 30, bottom: 100, right: 20 }
+    margin:    { top: 80, left: 40, bottom: 100, right: 20 }
 });
